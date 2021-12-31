89547f93ed48e50d34e5f0cc9c983e7f4628bdc9
Omer Can Korkmaz <omercankorkmaz0@gmail.com>
2021-12-31 16:18:38 +0300

    clear

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,11 @@
-import { NgModule, Self } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { DepOneComponent } from './dep-one/dep-one.component';
-import { FacultyService } from './faculty.service';
-import { ParentForHostDirective } from './parent-for-host.directive';
-import { ChildForHostDirective } from './child-for-host.directive';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DepOneComponent,
-    ParentForHostDirective,
-    ChildForHostDirective
+    AppComponent
   ],
   imports: [
     BrowserModule
